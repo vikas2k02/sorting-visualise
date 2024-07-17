19eadad08b27f6e9e9d1d39ed99840118c4d0b74
vikas <macbook@VikasMacbook.local>
2024-07-17 23:20:55 +0530

    sucess

--- js/beep.js
+++ js/beep.js
@@ -8,7 +8,7 @@ function beep(vol, freq, duration) {
     v.frequency.value = freq
     v.type = "square"
     u.connect(a.destination)
-    u.gain.value = vol * 0.01
+    u.gain.value = vol * 0
     v.start(a.currentTime)
     v.stop(a.currentTime + duration * 0.001)
 }
